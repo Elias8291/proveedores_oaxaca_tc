732f2feb41f1d236f6268bb66b55e80a1f057884
JacquelinePensamiento <jacquempd@gmail.com>
2025-04-03 21:26:23 -0600

    tabla deusuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -28,6 +28,7 @@
                 </tr>
             </thead>
             <tbody>
+                
                 @foreach ($users as $user)
                     <tr>
                         <td>
